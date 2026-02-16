f5665139b56bfd53ab93a145840ae7b6ca13a3a8
kashikaryash <kashikaryash@gmail.com>
2026-02-16 23:07:01 +0530

    debug: log port and revert server.port to

--- src/main/java/com/EmployeeManagementSystemApplication.java
+++ src/main/java/com/EmployeeManagementSystemApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmployeeManagementSystemApplication {
 
 	public static void main(String[] args) {
+		System.out.println("DEBUG: PORT Env Var = " + System.getenv("PORT"));
 		SpringApplication.run(EmployeeManagementSystemApplication.class, args);
 	}
 
